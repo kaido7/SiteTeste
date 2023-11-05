2da45edab180088f62ee284e2d175144b96b136a
Gabriel <gabrielsribeiro9999@gmail.com>
2023-11-05 15:33:49 -0300

    as

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
+  
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
+  
   const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
+  
   if (active) {
     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
   } else {
@@ -18,3 +21,4 @@ function toggleMenu(event) {
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
+ 
